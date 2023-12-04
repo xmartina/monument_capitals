74e07775591d420f5e937a9089984c70135a4cf7
xmartina <martinsprince126@gmail.com>
2023-12-04 03:04:22 +0100

    init

---
+++ template/asset/theme1/frontend/css/color.php
@@ -0,0 +1,48 @@
+
+:root {
+    --main-color: #d77600;
+}
+
+.feature-box .content {
+    box-shadow: inset 0 0px 66px #d7760047;
+}
+
+.invest-plan-top::before {
+    box-shadow: inset 0 0px 66px #d77600d4;
+}
+
+.invest-plan::before {
+    box-shadow: inset 0 0px 66px #d776003b;
+}
+
+.invest-plan-shape::after {
+    background-color: #d7760033;
+}
+
+.invest-plan:hover .invest-plan-shape::after {
+    background-color: #d77600;
+}
+
+.work-box {
+    box-shadow: inset 0 0px 20px #d77600c4;
+}
+
+.work-box .icon {
+    box-shadow: inset 0 0px 20px #d77600c4;
+}
+
+.cmn-table.style-separate tbody tr {
+    box-shadow: inset 0 0px 8px #d776004d;
+}
+
+.testimonial-box .content {
+    box-shadow: inset 0 0px 20px #d77600c4;
+}
+
+.referral-box::before {
+    box-shadow: inset 0 0px 20px #d77600c4;
+}
+
+.referral-box-step {
+    box-shadow: inset 0 0px 6px #d776009e;
+}
